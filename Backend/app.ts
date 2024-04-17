93717e71b0c42a629421d45bc5365c2c126699bb
Rajan <rajan0041sd@gmail.com>
2024-04-17 22:37:32 +0530

    inital commit

---
+++ Backend/app.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { makeExecutableSchema } from "graphql-tools";
+
+const app: express.Application = express();
+
+const PORT = 3001;
+
+let typeDefs: any = [`
+    type Query {
+        hello: String
+    }
+
+    type Mutation {
+        hello(Message: String): String
+    }
+`]
+
+let helloMessage: String = 'World';
+
+let resolvers = {
+    Query: {
+        hello: () => helloMessage
+    },
+    Mutation: {
+        hello: (_: any, helloData: any) => {
+            helloMessage = helloData.message;
+            return helloMessage
+        }
+    }
+}
+
+app.use(
+    'graphql',
+    graphqlHTTP({
+        schema: makeExecutableSchema({typeDefs, resolvers}),
+        graphiql: true
+    })
+)
+
+app.listen(PORT, () => console.log(`Node Graphql API listening on port ${PORT}!`))
